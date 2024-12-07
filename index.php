8a7731fa452039833b0007b9cc706284db8b853a
leofperes <leonardofperess@outlook.com>
2024-12-06 21:26:43 -0300

    exercise to check if its possible to be a blood donor

--- index.php
+++ index.php
@@ -5,61 +5,21 @@
     </head>
 
     <body>
-        <?php
-            //string
-            $firstName = 'leonardo';
+    <?php
+        $nome = 'Leonardo';
+        $idade = 19;
+        $peso_corporal = 76.5;
+        $doador = false;
 
-            //int
-            $age = 23;
-
-            //float
-            $weight = 76.5;
-
-            //
-            $overweight = true;
-        ?>
-        <?php
-            //comentário de uma linha
-            echo 'Name: '.$firstName.'<br>';
-            echo "Meu nome é $firstName<br/>";
-            #comentário de uma linha estilo shell
-            print 'oi'
-
-            /* comentario de multiplas linhas
-               este tipo de comentario permite que varias linhas
-               sejam comentadas ao mesmo tempo
-            */
-        ?>
-        <h1>Ficha cadastral</h1>
-        <p>Nome: <?php echo $firstName ?></p>
-        <p>Idade: <?php echo $age ?></p>
-        <p>Peso: <?php echo $weight ?></p>
-        <p>Sobrepeso: <?php echo $overweight ?></p>
-
-        <?php
-            define('BD_URL', 'br_bd_dev');
-            echo BD_URL;
-        ?>
-
-        <?php
-            $cartao_loja = true;
-            $valor_compra = 100;
-
-            $valor_frete = 50;
-            $desconto_frete = false;
-
-            $valor_frete_aux = $cartao_loja && $valor_compra >= 400 ? 0 : ($cartao_loja && $valor_compra >= 300 ? 10 : ($cartao_loja && $valor_compra >= 100 ? 25 : $valor_frete));
-            $desconto_frete = $valor_frete != $valor_frete_aux ? true : false
-        ?>
-        <h2>Detalhes do pedido</h2>
-        <p>Possui cartão da loja? <?= $cartao_loja ? 'Sim' : 'Não'; ?>
-
-        <p>Valor da compra: <?php echo $valor_compra ?></p>
-        <p>Recebeu desconto no frete?
-            <?php
-                $teste = $desconto_frete ? 'Sim' : 'Não';
-                echo $teste;
-            ?></p>
-        <p>Valor do frete: <?php echo $valor_frete ?></p>
+        if(($idade > 16 && $idade < 69) && $peso_corporal >= 50){
+            $doador = true;
+        }
+    ?>
+        <h1>Bem vindo a página de doação de sangue</h1>
+        <p>Vamos verificar se você atende aos requisitos para ser um doador!<br/></p>
+        <p>Nome: <?= $nome ?></p>
+        <p>Idade: <?= $idade ?></p>
+        <p>Peso: <?= $peso_corporal ?></p>
+        <p><?php echo $doador == true ? 'Atende aos requisitos' : 'Não atende aos requisitos' ?>
     </body>
 </html>
